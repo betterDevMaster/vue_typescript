cdb66b00ad9ccee5558ef350758f6b446c92990d
SeniorDEVMaster <rj.accout119@gmail.com>
2020-03-30 10:46:31 +0800

    initial comment

---
+++ dist/precache-manifest.dae57de2329413b65bb20c288f78ae95.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b61b9a4622a5aa22b337",
+    "url": "/css/app.56714c3c.css"
+  },
+  {
+    "revision": "4cf0abb2319f9336adb461b65fdda21a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b61b9a4622a5aa22b337",
+    "url": "/js/app.00c3e4f1.js"
+  },
+  {
+    "revision": "e5598b8784e4b90ee761",
+    "url": "/js/chunk-vendors.f3306027.js"
+  },
+  {
+    "revision": "d0972b7b1ac67a6b2506a1a08f586a6d",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/robots.txt"
+  }
+]);
